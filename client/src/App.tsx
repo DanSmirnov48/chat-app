e7ffe5fbe95aeb914ec61679d51c916d3f8f978d
Dan <daniel.smirnov17@gmail.com>
2024-03-27 15:19:14 +0000

    added: (Client) Signed in User Nav & toast notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import { SignIn, SignUp } from "./_auth/forms";
+import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster expand={false} position="top-right" richColors closeButton className="" />
     </main>
   );
 }
---
+++ client/src/components/UserNav.tsx
@@ -0,0 +1,98 @@
+import { useTheme } from "next-themes";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button, buttonVariants } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Laptop,
+  LayoutDashboard,
+  LogOut,
+  Moon,
+  Palette,
+  Settings,
+  SunMedium,
+} from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queries/auth";
+
+export function UserNav() {
+  const { mutate: signOut } = useSignOutAccount();
+  const { user, setUser, setIsAuthenticated } = useUserContext();
+  const { setTheme } = useTheme();
+  const navigate = useNavigate();
+
+  const handleSignOut = async (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    signOut();
+    setIsAuthenticated(false);
+    setUser(INITIAL_USER);
+    navigate("/");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-10 w-10">
+            <AvatarImage
+              src={""}
+              alt={user.email}
+              className="object-cover"
+            />
+            <AvatarFallback>F U</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-xs leading-none text-muted-foreground">
+              Signed in as
+            </p>
+            <p className="text-sm font-medium leading-none">{user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Palette className="mr-2 h-4 w-4" />
+            <span>Theme</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                <SunMedium className="mr-2 h-4 w-4" />
+                <span>Light</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <Moon className="mr-2 h-4 w-4" />
+                <span>Dark</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                <Laptop className="mr-2 h-4 w-4" />
+                <span>System</span>
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={(e) => handleSignOut(e)}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
