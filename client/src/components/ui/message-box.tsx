80f7a2288f7a713a431993d49c099d56b42ae24b
Dan <daniel.smirnov17@gmail.com>
2024-04-02 12:26:47 +0100

    added: (Client) Context menu on MessageBox right click

--- client/src/components/ui/message-box.tsx
+++ client/src/components/ui/message-box.tsx
@@ -3,8 +3,20 @@ import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { IMessage } from "@/types";
 import { format } from "date-fns";
+import {
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+} from "@/components/ui/context-menu"
 
-const messageBoxVariants = cva("py-2 px-3 rounded-lg", {
+const messageBoxVariants = cva("py-2 px-3 rounded-lg z-99", {
     variants: {
         align: {
             self: "col-start-4 col-end-13 p-0",
@@ -32,28 +44,51 @@ const MessageBox = forwardRef<HTMLDivElement, MessageBoxProps>(
     ({ className, message, isSelf, ...props }, ref) => {
         const formattedTime = format(new Date(message.createdAt), "HH:mm");
         return (
-            <div
-                className={cn(
-                    messageBoxVariants({ align: isSelf ? "self" : "other", ...props }),
-                    className
-                )}
-                ref={ref}
-                {...props}
-            >
-                <div
-                    className={messageBoxVariants({ avatar: isSelf ? "self" : "other" })}
-                >
-                    <div
-                        className={cn(
-                            "relative text-sm py-2 px-4 shadow rounded-xl",
-                            messageBoxVariants({ textStyle: isSelf ? "self" : "other" })
-                        )}
-                    >
-                        <div className="mb-1 mr-10">{message.content}</div>
-                        <div className="absolute bottom-1.5 right-1.5 text-[0.65rem] leading-3">
-                            {formattedTime}
-                        </div>
-                    </div>
+            <div className={cn(messageBoxVariants({ align: isSelf ? "self" : "other", ...props }), className)} ref={ref}{...props}>
+                <div className={messageBoxVariants({ avatar: isSelf ? "self" : "other" })}>
+                    <ContextMenu>
+                        <ContextMenuTrigger asChild>
+                            <div className={cn("relative text-sm py-2 px-4 shadow rounded-xl", messageBoxVariants({ textStyle: isSelf ? "self" : "other" }))} >
+                                <div className="mb-1 mr-10">{message.content}</div>
+                                <div className="absolute bottom-1.5 right-1.5 text-[0.65rem] leading-3">
+                                    {formattedTime}
+                                </div>
+                            </div>
+                        </ContextMenuTrigger>
+                        <ContextMenuContent className="w-64">
+                            <ContextMenuItem inset>
+                                Back
+                                <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+                            </ContextMenuItem>
+                            <ContextMenuItem inset disabled>
+                                Forward
+                                <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+                            </ContextMenuItem>
+                            <ContextMenuItem inset>
+                                Reload
+                                <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+                            </ContextMenuItem>
+                            <ContextMenuSub>
+                                <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
+                                <ContextMenuSubContent className="w-48">
+                                    <ContextMenuItem>
+                                        Save Page As...
+                                        <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+                                    </ContextMenuItem>
+                                    <ContextMenuItem>Create Shortcut...</ContextMenuItem>
+                                    <ContextMenuItem>Name Window...</ContextMenuItem>
+                                    <ContextMenuSeparator />
+                                    <ContextMenuItem>Developer Tools</ContextMenuItem>
+                                </ContextMenuSubContent>
+                            </ContextMenuSub>
+                            <ContextMenuSeparator />
+                            <ContextMenuCheckboxItem checked>
+                                Show Bookmarks Bar
+                                <ContextMenuShortcut>⌘⇧B</ContextMenuShortcut>
+                            </ContextMenuCheckboxItem>
+                            <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
+                        </ContextMenuContent>
+                    </ContextMenu>
                 </div>
             </div>
         );
