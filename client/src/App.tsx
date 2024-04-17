e819f841b088b26b357642151e8635795cd0805b
Dan <daniel.smirnov17@gmail.com>
2024-04-17 15:49:08 +0100

    refactor: (Client) Removed old auth routes and file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
-import AuthLayout from "./_auth/AuthLayout";
-import { SignIn, SignUp } from "./_auth/forms";
 import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
     <main className="flex">
       <Routes>
-        {/* Auth Routes */}
-        <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Route>
-
-        {/* Public Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
         </Route>
--- client/src/_auth/forms/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as SignIn } from './SignInForm'
-export {default as SignUp } from './SignUpForm'
